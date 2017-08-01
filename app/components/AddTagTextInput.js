2f9c5e4520d080681fd65e86fec2ad42b322aa8e
Kola <kolapo.abolarinwa@slimmingworld.co.uk>
2017-08-01 16:37:09 +0100

    DropDownMenu dev.
    Started working on the drop down menu for the tag tex box

--- app/components/AddTagTextInput.js
+++ app/components/AddTagTextInput.js
@@ -1,33 +1,90 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import DropDownMenu from './DropDownMenu';
 
 class AddTagTextInput extends React.Component
 {
     constructor(props)
     {
         super(props);
+
+        this.state= {
+            dropDownMenuList:
+            [
+                {
+                    id: 1,
+                    name: "Kola",
+                    type: "consultant",
+                    image:null
+                },
+                 {
+                    id: 2,
+                    name: "Tola",
+                    type: "consultant",
+                    image:null
+                },
+                  {
+                    id: 3,
+                    name: "Roy",
+                    type: "consultant",
+                    image:null
+                }
+            ],
+            showDropDownMenu:"none"
+        }
+        this.onChangeDistributor = this.onChangeDistributor.bind(this);
+    }
+
+    onChangeDistributor(event) {
+        this.props.onChange(event);
+        let e = event.target.value;
+        console.log("onChangeDistributor "+ e);
+        if (e != null &&  e.length>0) {
+            this.setState(() => {
+              return   ( {
+                    showDropDownMenu:
+                        "block"
+                })
+                   
+            });
+        }
+        if (e.length<=0) 
+        {
+            this.setState(() => {
+               return {
+                    showDropDownMenu:
+                       "none"
+               }
+                   console.log("onChangeDistributor after "+ prevState.showDropDownMenu);
+               
+            });
+        }
     }
 
     render()
     {
         return (
-            <input
-               
-                id="txtAddATag"
-                type="text" name="txtAddATag"
-                placeholder={this.props.placeHolder ? this.props.placeHolder : "+ Add label"}
-                maxLength={this.props.maxLength ? this.props.maxLength : 23}
-                style={{
-                    //you can set properties directly using props as well
-                    width: this.props.width ? this.props.width : "100%",
-                    borderRadius: this.props.borderRadius ? this.props.borderRadius : 5,
-                    paddingRight: this.props.paddingRight ? this.props.paddingRight : 10,
-                    outlineColor: this.props.outlineColor,
-                    textIndent: this.props.textIndent ? this.props.textIndent : 5,
-                    boxSizing: "border-box",
-                    height: this.props.height ? this.props.height : 25
-                }}
-                onChange={this.props.onChange} />
+            <div className="dropDown">
+                <input
+
+                    id="txtAddATag"
+                    type="text" name="txtAddATag"
+                    placeholder={this.props.placeHolder ? this.props.placeHolder : "+ Add label"}
+                    maxLength={this.props.maxLength ? this.props.maxLength : 23}
+                    style={{
+                        //you can set properties directly using props as well
+                        width: this.props.width ? this.props.width : "100%",
+                        borderRadius: this.props.borderRadius ? this.props.borderRadius : 5,
+                        paddingRight: this.props.paddingRight ? this.props.paddingRight : 10,
+                        outlineColor: this.props.outlineColor,
+                        textIndent: this.props.textIndent ? this.props.textIndent : 5,
+                        boxSizing: "border-box",
+                        height: this.props.height ? this.props.height : 25
+                    }}
+                    onChange={this.onChangeDistributor.bind(this)} />
+
+               <DropDownMenu displayType={this.state.showDropDownMenu} dropDownMenuList={this.state.dropDownMenuList}/>
+            </div>
         );
     }
 }
